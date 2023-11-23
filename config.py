9bb7c98c98831a8188957835b274453b11702954
Max Oliver <110696335+maxoliverbr@users.noreply.github.com>
2023-11-22 23:12:06 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """
 Config file with API_KEY, URL and review locator.
 """
-API_KEY = "AIzaSyBP_8z_pU3qFgMAPDlqzNpV1qQIYjFevh0"
+API_KEY = "GET_API_KEY"
 URL_REVIEWS = "https://www.google.com/maps/place/Good+Guys+Automotive+LLC/@28.5123901,-81.3987442,17z/data=!4m8!3m7!1s0x88e77b9740577a69:0xb8022f79d896a07d!8m2!3d28.5123901!4d-81.3961693!9m1!1b1!16s%2Fg%2F11cr_f82qz?hl=en&entry=ttu"
 REVIEW_LOCATOR = "//span[@class='wiI7pd']"
 
